5708503bf695f5b3d899634048b9b9dd27010290
Chitsanupong <first30952@gmail.com>
2021-09-17 11:10:01 +0700

    feat: create component via manual and CLI

--- angular-learning/src/app/app.module.ts
+++ angular-learning/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, TodoComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
