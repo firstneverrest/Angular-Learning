1279ee74cbf121343a35f6df31e085fe7f52b6f2
Chitsanupong <first30952@gmail.com>
2021-09-17 12:43:01 +0700

    feat: data binding

--- angular-learning/src/app/todo/todo.component.ts
+++ angular-learning/src/app/todo/todo.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  todoList = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onEditTodo(): void {
+    this.todoList = 'Studying in the evening';
   }
 
+  onUpdateTodoName(event: Event): void {
+    this.todoList = (<HTMLInputElement>event.target).value;
+  }
 }
