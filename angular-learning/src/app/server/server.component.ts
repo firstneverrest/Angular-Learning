17d1ccc16ef1f83bdcae399a80b9035f75d110da
Chitsanupong <first30952@gmail.com>
2021-09-17 11:51:07 +0700

    feat: data binding

--- angular-learning/src/app/server/server.component.ts
+++ angular-learning/src/app/server/server.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
-  title = 'server';
+  title: string = 'server';
+  serverId: number = 101;
+  serverStatus: string = 'online';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
